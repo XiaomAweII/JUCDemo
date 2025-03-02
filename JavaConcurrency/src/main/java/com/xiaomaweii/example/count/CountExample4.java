3442f6e5e1b8b5524ffa13747356c8ed067a0c01
XiaomAweII <805122541@qq.com>
2025-03-02 15:43:22 +0800

    4.3 线程安全性-可见性

---
+++ JavaConcurrency/src/main/java/com/xiaomaweii/example/count/CountExample4.java
@@ -0,0 +1,53 @@
+package com.xiaomaweii.example.count;
+
+import com.xiaomaweii.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 使用volatile修饰验证能否保证线程安全
+ *
+ * @author xiaoweii
+ * @create 2025-03-01 23:27
+ */
+@Slf4j
+@NotThreadSafe
+public class CountExample4 {
+
+    // 请求总数
+    public static int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static volatile int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        exec.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private static void add() {
+        count++;
+    }
+}
