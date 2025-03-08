0c1cde720f6d99f69703ca02a137a3813239cba3
XiaomAweII <805122541@qq.com>
2025-03-09 00:03:21 +0800

    7.3 JUC之AQS - ReentrantLock与锁

---
+++ JavaConcurrency/src/main/java/com/xiaomaweii/example/lock/LockExample2.java
@@ -0,0 +1,68 @@
+package com.xiaomaweii.example.lock;
+
+import com.xiaomaweii.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author xiaoweii
+ * @create 2025-03-08 23:16
+ */
+@Slf4j
+@ThreadSafe
+public class LockExample2 {
+    // 请求总数
+    private static int threadTotal = 200;
+
+    // 同时并发执行的线程数
+    private static int clientTotal = 5000;
+
+    private static int count = 0;
+
+    // 声明锁
+    private final static Lock lock = new ReentrantLock();
+
+    /**
+     * 使用线程池和信号量来模拟客户端请求
+     *
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // 线程池
+        ExecutorService exec = Executors.newCachedThreadPool();
+        // 信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int index = 0; index < clientTotal; index++) {
+            exec.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        exec.shutdown();
+        log.info("count:{}", count);
+    }
+
+    private synchronized static void add() {
+        lock.lock();;
+        try {
+            count++;
+        }finally {
+            lock.unlock();
+        }
+    }
+
+}
