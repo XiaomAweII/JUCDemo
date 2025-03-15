dd1e27ac98efc9c7e13efe1405868961a2e975a5
XiaomAweII <805122541@qq.com>
2025-03-15 15:17:14 +0800

    高并发学习初始化

---
+++ HighConcurrency/src/main/java/com/xiaomaweii/ConcurrencyApplication.java
@@ -0,0 +1,15 @@
+package com.xiaomaweii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaoweii
+ * @create 2025-03-15 15:14
+ */
+@SpringBootApplication
+public class ConcurrencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class,args);
+    }
+}
